3d9c19b79fda980f8dab3d3d42390a02355b35bd
Mykola Marchuk <mmarchuk@Mykolas-MacBook-Pro.local>
2025-07-08 06:47:19 +0300

    implement graceful shutdown

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/marchuknikolay/rss-parser/internal/config"
 	"github.com/marchuknikolay/rss-parser/internal/server"
@@ -25,16 +30,34 @@ func main() {
 		log.Fatalf("failed creating a new database connection: %v", err)
 	}
 
-	defer storage.Close()
-
 	service := service.New(storage)
 
 	handler := handlers.New(service)
 
 	server := server.New("8080", handler.InitRoutes())
 
-	err = server.Start()
-	if err != nil {
-		log.Fatalf("failed starting server: %v", err)
+	go func() {
+		if err = server.Start(); err != nil {
+			log.Fatalf("failed starting server: %v", err)
+		}
+	}()
+
+	log.Println("Server is running on port 8080")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Println("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("failed shutting down server: %v", err)
 	}
+
+	storage.Close()
+
+	log.Println("Server stopped gracefully")
 }

3ff13e512ade5c7b64a62710bb1ed0cd0f0d9bc6
Mykola Marchuk <mmarchuk@Mykolas-MacBook-Pro.local>
2025-07-08 23:31:40 +0300

    remove unused functions from the storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,12 +2,10 @@ package storage
 
 import (
 	"context"
-	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/marchuknikolay/rss-parser/internal/model"
 )
 
 type RowQueryer interface {
@@ -74,80 +72,4 @@ func (s *Storage) ExecExecutor() CommandExecutor {
 	}
 
 	return s.Pool
-}
-
-func (s *Storage) FetchItemsByChannelId(channelId int) ([]model.Item, error) {
-	rows, err := s.Pool.Query(context.Background(),
-		"SELECT id, title, description, pub_date FROM items WHERE channel_id = $1", channelId)
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	items := []model.Item{}
-
-	for rows.Next() {
-		var (
-			id                 int
-			title, description string
-			pubDate            time.Time
-		)
-
-		if err := rows.Scan(&id, &title, &description, &pubDate); err != nil {
-			return nil, err
-		}
-
-		items = append(items, model.Item{Id: id, Title: title, Description: description, PubDate: model.DateTime(pubDate)})
-	}
-
-	return items, nil
-}
-
-func (s *Storage) FetchChannels() ([]model.Channel, error) {
-	rows, err := s.Pool.Query(context.Background(), "SELECT id, title, language, description FROM channels")
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	channels := []model.Channel{}
-
-	for rows.Next() {
-		var (
-			id                           int
-			title, language, description string
-		)
-
-		if err := rows.Scan(&id, &title, &language, &description); err != nil {
-			return nil, err
-		}
-
-		items, err := s.FetchItemsByChannelId(id)
-		if err != nil {
-			return nil, err
-		}
-
-		channels = append(channels, model.Channel{Id: id, Title: title, Language: language, Description: description, Items: items})
-	}
-
-	return channels, nil
-}
-
-func (s *Storage) FetchItemById(id int) (model.Item, error) {
-	var (
-		title, description string
-		pubDate            time.Time
-	)
-
-	err := s.Pool.QueryRow(context.Background(),
-		"SELECT title, description, pub_date FROM items WHERE id = $1", id).
-		Scan(&title, &description, &pubDate)
-
-	if err != nil {
-		return model.Item{}, err
-	}
-
-	return model.Item{Id: id, Title: title, Description: description, PubDate: model.DateTime(pubDate)}, nil
 }

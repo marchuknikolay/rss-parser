487da9e20f779494a8485b65f9dca3d1928c6d2d
Mykola Marchuk <mmarchuk@Mykolas-MacBook-Pro.local>
2025-07-28 15:53:14 +0300

    cover fetcher with tests

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/lib/pq v1.10.9
 	github.com/pressly/goose v2.7.0+incompatible
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
@@ -24,9 +25,14 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/fetcher/fetcher_test.go
@@ -0,0 +1,43 @@
+package fetcher
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFetch_Success(t *testing.T) {
+	const content = "Content"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, content)
+	}))
+	defer server.Close()
+
+	bs, err := Fetch(server.URL)
+
+	require.NoError(t, err)
+	require.Equal(t, content, string(bs))
+}
+
+func TestFetch_NetworkError(t *testing.T) {
+	_, err := Fetch("https://invalid.url")
+
+	require.Error(t, err)
+}
+
+func TestFetch_StatusNotOK(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		http.Error(w, "InternalServerError", http.StatusInternalServerError)
+	}))
+	defer server.Close()
+
+	bs, err := Fetch(server.URL)
+
+	require.Nil(t, bs)
+	require.Error(t, err)
+}
